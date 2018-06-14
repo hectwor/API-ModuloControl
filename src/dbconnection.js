29dd4093d1b04fd6eeb982905748265a6eff27cc
Héctor Daniel Huapaya Vásquez <hectorh.danielv@gmail.com>
2018-06-13 20:41:32 -0500

    New BD

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -7,7 +7,7 @@ let pgp = require('pg-promise')(options);
 
 //const connectString = 'postgres://postgres:1234@localhost/recaudaciones';
 //const urlconnection = 'postgres://modulo4@sigap.postgres.database.azure.com:modulo4@sigap.postgres.database.azure.com:5432/tcs';
-const  urlconnection = 'postgres://postgres:postgres@sigap-second.cbzwydm3f2if.sa-east-1.rds.amazonaws.com:5432/tcs';
+const  urlconnection = 'postgres://postgres:postgres@sigap-second.cbzwydm3f2if.sa-east-1.rds.amazonaws.com:5432/tcs2';
 let cn = pgp(urlconnection);
 
 module.exports = {
